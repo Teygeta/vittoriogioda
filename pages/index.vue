0ee0b8f80e75b1f949cf9fee0705244f32b40a1f
Vittorio <98349901+Teygeta@users.noreply.github.com>
2023-11-15 17:28:47 +0100

    metatag

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+useSeoMeta({
+  title: 'teygenesis',
+  description: '',
+  ogTitle: '',
+  ogDescription: '',
+  ogImage: '',
+  ogUrl: '',
+})
 
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+})
 </script>
 
 <template>
