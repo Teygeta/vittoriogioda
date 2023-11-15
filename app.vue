d0c36d1850bcb6d036893a684b8a0ad64fd1284d
Vittorio <98349901+Teygeta@users.noreply.github.com>
2023-11-15 17:29:38 +0100

    fix: metatag

--- app.vue
+++ app.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'teygenesis',
+  description: '',
+  ogTitle: '',
+  ogDescription: '',
+  ogImage: '',
+  ogUrl: '',
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+})
+
+</script>
+
 <template>
   <div>
     <NuxtPage />
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,5 @@
 <script setup lang="ts">
-useSeoMeta({
-  title: 'teygenesis',
-  description: '',
-  ogTitle: '',
-  ogDescription: '',
-  ogImage: '',
-  ogUrl: '',
-})
 
-useHead({
-  htmlAttrs: {
-    lang: 'en',
-  },
-})
 </script>
 
 <template>
