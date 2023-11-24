dfb881471990db490d7336cd0939dc606e6536b1
Vittorio <98349901+Teygeta@users.noreply.github.com>
2023-11-24 18:20:36 +0100

    fix: dark mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode'
   ],
+  colorMode: {
+    classSuffix: ''
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
