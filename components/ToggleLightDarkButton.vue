64687b6bdb5ad1da7643857ba804bf87ad8bfa90
Vittorio <98349901+Teygeta@users.noreply.github.com>
2023-11-25 11:50:52 +0100

    fix: icon transition

--- components/ToggleLightDarkButton.vue
+++ components/ToggleLightDarkButton.vue
@@ -16,10 +16,10 @@ const isDark = computed({
 <template>
   <ClientOnly>
     <button
-      class="rounded border border-transparent px-2.5 py-1.5 text-gray-800 hover:border-gray-500/10 hover:bg-gray-200/30 dark:text-gray-200 dark:hover:bg-gray-800/30"
+      class="rounded border group border-transparent px-2.5 py-1.5 text-gray-800 hover:border-gray-500/10 hover:bg-gray-200/30 dark:text-gray-200 dark:hover:bg-gray-800/30"
       aria-label="Theme" type="button" @click="isDark = !isDark">
-      <MoonIcon v-if="isDark" class="flex-shrink-0 w-5 h-5" />
-      <SunIcon v-else class="flex-shrink-0 w-5 h-5" />
+      <MoonIcon v-if="isDark" class="flex-shrink-0 w-5 h-5 group-hover:rotate-[20deg] transition" />
+      <SunIcon v-else class="flex-shrink-0 w-5 h-5 group-hover:rotate-[20deg] transition" />
     </button>
 
     <template #fallback>
