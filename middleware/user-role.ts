b31ac1c6d92339b5f6a0f630615f8af4f6dcd640
Vittorio <98349901+Teygeta@users.noreply.github.com>
2024-01-16 12:21:11 +0100

    Refactor user role middleware logic

--- middleware/user-role.ts
+++ middleware/user-role.ts
@@ -1,27 +1,38 @@
 export default defineNuxtRouteMiddleware((to) => {
-    const { status } = useAuth()
-    const user = useAuthUser()
+	const { status } = useAuth()
+	const user = useAuthUser()
 
-    const userRole = user.value.role
-    // Return immediately if user is already authenticated
-    if (status.value === 'authenticated') {
-        if (userRole === 'ADMIN') {
-            return
-        } else if (userRole === 'AUTHOR') {
-            if (to.path !== '/blog/create-post') {
-                return navigateTo('/admin')
-            }
-        } else if (userRole === 'MODERATOR') {
-            if (to.path !== '/blog/index') {
-                return navigateTo('/admin')
-            }
-        } else if (userRole === 'USER') {
-            // HACK: User role has no rules yet
-            return navigateTo('/admin')
-        }
-        else if (!userRole) {
-            return navigateTo('/admin')
-        }
-    }
+	const userRole = user.value.role
+	// Return immediately if user is already authenticated
+	if (status.value === 'authenticated') {
+		if (userRole === 'ADMIN') {
+			return
+		}
+
+		else if (to.path === '/admin/settings') {
+			return
+		}
+
+		else if (!userRole) {
+			return navigateTo('/admin')
+		}
+
+		else if (userRole === 'AUTHOR') {
+			if (to.path !== '/admin/blog/create-post') {
+				return navigateTo('/admin')
+			}
+		}
+
+		else if (userRole === 'MODERATOR') {
+			if (to.path !== '/admin/blog/index') {
+				return navigateTo('/admin')
+			}
+		}
+
+		else if (userRole === 'USER') {
+			// HACK: User role has no rules yet
+			return navigateTo('/admin')
+		}
+	}
 
 })
