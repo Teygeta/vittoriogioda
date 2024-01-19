5715f1eb8636e6aa1adebded3abb83b466262b60
Vittorio <98349901+Teygeta@users.noreply.github.com>
2024-01-19 12:26:39 +0100

    search bar for posts

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
 const { $trpc } = useNuxtApp()
+const query = ref('')
 
 const { data } = await $trpc.user.blog.paginatePosts.useQuery()
 
 const posts = computed(() => data.value?.posts ?? [])
+
+const filteredPosts = computed(() => {
+  if (!query.value) return posts.value
+  // filter posts by title and content
+  return posts.value.filter((post) => {
+    const title = post.title.toLowerCase()
+    const content = post.content?.toLowerCase()
+    const q = query.value.toLowerCase()
+    return title.includes(q) || content?.includes(q)
+  })
+})
 </script>
 
 <template>
   <main>
     <div class="flex flex-col max-w-3xl gap-5 mx-auto">
-      <nuxt-link
-        v-for="post in posts"
-        :key="post.id" class="px-6 py-3 transition rounded-md dark:hover:bg-opacity-10 dark:bg-white dark:bg-opacity-5" :to="`/blog/${post.id}`"
-      >
+      <div>
+        <Input type="text" v-model.trim="query" placeholder="Cerca un post..." />
+      </div>
+
+      <nuxt-link v-if="filteredPosts.length > 0" v-for="post in filteredPosts" :key="post.id"
+        class="px-6 py-3 transition rounded-md dark:hover:bg-opacity-10 dark:bg-white dark:bg-opacity-5"
+        :to="`/blog/${post.id}`">
         <div>
           <h1 class="text-2xl font-semibold">
             {{ post.title }}
@@ -20,6 +35,13 @@ const posts = computed(() => data.value?.posts ?? [])
           <div class="post-container" v-html="post.content" />
         </div>
       </nuxt-link>
+
+      <div v-else class="text">
+        <p class="text-sm text-muted-foreground">
+          Nessun post trovato
+        </p>
+      </div>
+
     </div>
   </main>
 </template>
