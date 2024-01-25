c0a6ccb631c2d3b045ebb1d10ca92f7ec489dea8
Vittorio <98349901+Teygeta@users.noreply.github.com>
2024-01-25 19:45:08 +0100

    Refactor blog post display logic

--- pages/blog/[postId]/index.vue
+++ pages/blog/[postId]/index.vue
@@ -17,44 +17,15 @@ const { data } = await $trpc.user.blog.getPostById.useQuery({
 
 const post = computed(() => data.value?.post)
 
-const isDraftPostAuthor = computed(() => {
-  if (post.value) {
-    if (route.query.draft) {
-      return route.query.draft === post.value.authorId
-    }
-    else {
-      return false
-    }
-  }
+useHead({
+  title: post.value?.title,
 })
+
 </script>
 
 <template>
   <div>
-    <main v-if="post && isDraftPostAuthor" class="max-w-4xl mx-auto">
-      <Alert class="!border-yellow-300">
-        <PencilLine class="h-4 w-4" />
-        <AlertTitle>Draft post</AlertTitle>
-        <AlertDescription>
-          Il tuo post è ancora in bozza, non è ancora stato pubblicato.
-          Attendi che un amministratore lo pubblichi.
-        </AlertDescription>
-      </Alert>
-
-      <div class="my-8">
-        <h1 class="mb-4 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          {{ post.title }}
-        </h1>
-        <div class="font-mono text-gray-500">
-          {{ post.author.name }} -
-          {{ format(post.createdAt, 'PPPP', { locale: it }).replace(/^\w/, (c) => c.toUpperCase()) }}
-        </div>
-      </div>
-
-      <div class="post-container" v-html="post.content" />
-    </main>
-
-    <main v-else-if="post && post.published" class="max-w-4xl mx-auto">
+    <main v-if="post && post.published" class="max-w-4xl mx-auto">
       <div class="my-8">
         <h1 class="mb-4 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           {{ post.title }}
